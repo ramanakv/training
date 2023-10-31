1799ee7eba2b1a9010f9bae66002a2a7b5605bc3
Ramana Reddy <ramanakv@ymail.com>
2023-10-31 08:58:03 +0530

    adding persistence xml

--- layeredappproject/src/com/cg/trg/lapp/exceptions/DuplicateEmployeeException.java
+++
@@ -1,9 +0,0 @@
-package com.cg.trg.lapp.exceptions;
-
-public class DuplicateEmployeeException extends Exception{
-
-	public DuplicateEmployeeException(String message) {
-		super(message);
-	}
-	
-}
