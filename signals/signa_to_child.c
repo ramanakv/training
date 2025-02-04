7198e0853545da285da7c3b670882148f42a8ef3
Ramana Reddy <ramanakv@ymail.com>
2025-02-04 17:43:40 +0530

    signals

--- signals/signa_to_child.c
+++ signals/signa_to_child.c
@@ -4,74 +4,61 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-// function declaration
-void sighup();
-void sigint();
-void sigquit();
+// signal handling functions
+void sighup()
+{
+    signal(SIGHUP, sighup); // reset signal
+    printf("CHILD: I have received a SIGHUP\n");
+}
+void sigint()
+{
+    signal(SIGINT, sigint); // reset signal
+    printf("CHILD: I have received a SIGINT\n");
+}
+void sigquit()
+{
+    printf("My DADDY has Killed me!!!\n");
+    exit(0);
+}
 
 void main()
 {
     int pid;
 
-    /* get child process */
-    if ((pid = fork()) < 0) {
+    signal(SIGHUP, sighup);
+    signal(SIGINT, sigint);
+    signal(SIGQUIT, sigquit);
+
+    if ((pid = fork()) < 0)
+    {
         perror("fork");
         exit(1);
     }
 
     /* child */
-    if (pid == 0) { 
-        signal(SIGHUP, sighup);
-        signal(SIGINT, sigint);
-        signal(SIGQUIT, sigquit);
-        for (;;)
-          
-            /* loop forever */
-            ; 
+    if (pid == 0)
+    {
+        while (1){ }
     }
 
-    /* parent */
-    else 
-      
+    else
+
     /* pid holds id of child */
-    { 
-        write(STDOUT_FILENO, "\nPARENT: sending SIGHUP\n\n", 25);
+    {
+        printf("PARENT: sending SIGHUP\n");
         kill(pid, SIGHUP);
 
-        /* pause for 3 secs */
-        sleep(3); 
-        write(STDOUT_FILENO, "\nPARENT: sending SIGINT\n\n", 25);
+        sleep(3);
+        printf("PARENT: sending SIGINT\n");
         kill(pid, SIGINT);
 
-        /* pause for 3 secs */
-        sleep(3); 
-        write(STDOUT_FILENO, "\nPARENT: sending SIGQUIT\n\n", 26);
+        sleep(3);
+        printf("PARENT: sending SIGQUIT\n");
         kill(pid, SIGQUIT);
+        
         sleep(3);
     }
 }
 
-// sighup() function definition
-void sighup()
-{
-  
-    /* reset signal */
-    signal(SIGHUP, sighup); 
-    write(STDOUT_FILENO, "CHILD: I have received a SIGHUP\n", 31);
-}
 
-// sigint() function definition
-void sigint()
-{
-    
-    /* reset signal */
-    signal(SIGINT, sigint); 
-    write(STDOUT_FILENO, "CHILD: I have received a SIGINT\n", 32);
-}
 
-// sigquit() function definition
-void sigquit()
-{
-    write(STDOUT_FILENO, "My DADDY has Killed me!!!\n", 26);
-    exit(0);
-}
